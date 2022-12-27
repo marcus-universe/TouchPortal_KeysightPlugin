41db193a6e16ff53cd6472786fca3bb6bf18f64a
Marcus Tutorials <marcusharting@web.de>
2022-12-27 20:27:47 +0100

    Fix Sending Issue to Keysight

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-const Client = require('socket.io-client');
+// const Client = require('socket.io-client');
 const TouchPortalAPI = require('touchportal-api');
 const TPClient = new TouchPortalAPI.Client();
 const pluginId = 'keysight';
-
-// Connect to the SocketIO server
-var socket = Client('ws://localhost:3000');
-
-// TPClient.on("Settings", (data) => {
-//   console.log(data[0] + " setting set");
-
-//     socket = Client(toString(data[0].value) + ":" + toString(data[1].value));
-// });
-
-TPClient.on("Action", (data) => {
-  if (data.actionId == "send_keysight_command") {
-    let command = data.data[0].value;
-    console.log(data + " was sent to Keysight");
-
-    // Send the command to the SocketIO server
-    socket.emit('command', command);
-  }
-});
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
 
 
+const io = require('socket.io')(3000, server, {cors: {origin: '*'}});
+
+// const socket = io('ws://localhost:3000');
+
+// socket.on('connect', () => { console.log('Client connected'); });
+
+  TPClient.on("Action", (data) => {
+    if (data.actionId == "send_keysight_command") {
+      let command = data.data[0].value;
+      console.log(command + " was sent to Keysight");
+
+      // Send the command to the SocketIO server
+      io.emit('command', command);
+    }
+  });
+
+  
 
 
 TPClient.connect({ pluginId });
