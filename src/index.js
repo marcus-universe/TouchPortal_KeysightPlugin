d47d7b942f63f1aa26e552087148ca5ea01c958c
Marcus Tutorials <marcusharting@web.de>
2022-12-27 17:10:16 +0100

    Added Server and Action with Message Send

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const socketIO = require('socket.io-client');
+const Client = require('socket.io-client');
 const TouchPortalAPI = require('touchportal-api');
 const TPClient = new TouchPortalAPI.Client();
 const pluginId = 'keysight';
 
 // Connect to the SocketIO server
-const socket = socketIO('http://localhost:3000');
+var socket = Client('ws://localhost:3000');
 
-TPClient.on("Settings",(data) => {
+// TPClient.on("Settings", (data) => {
+//   console.log(data[0] + " setting set");
 
-    socket = socketIO(data.data[0].value + ":" + data.data[1].value);
-    TPClient.removeState("stateIdToRemove");
-});
+//     socket = Client(toString(data[0].value) + ":" + toString(data[1].value));
+// });
 
 TPClient.on("Action", (data) => {
   if (data.actionId == "send_keysight_command") {
-    let command = message.data.data[0].value;
-    console.log(command + " was sent to Keysight");
+    let command = data.data[0].value;
+    console.log(data + " was sent to Keysight");
 
     // Send the command to the SocketIO server
     socket.emit('command', command);
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {cors: {origin: '*'}});
 
-server.listen(3000);
+
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
+    console.log('Client connected');
 
-  // Listen for incoming commands
-  socket.on('command', (command) => {
-    console.log(`Received command: ${command}`);
+    // Listen for incoming commands
+    socket.on('command', (command) => {
+        console.log(`Received command: ${command}`);
+        io.emit('command', command);
 
-    // Do something with the command here, such as sending it to a device or processing it in some way
-  });
+        // Do something with the command here, such as sending it to a device or processing it in some way
+    });
 });
+
+server.listen(3000, () => console.log('Server listening on port 3000'));
